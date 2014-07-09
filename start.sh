31172818d11ab5ecd04a88cc47718439403ab1cc
Juergen Hoffmann <hoffmann@apache.org>
2014-07-10 00:53:16 +0200

    enabling SSH to be able to SSH into the containers

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -p 49260:8080 -p 49270:9990 -h fsw --name fsw -d psteiner/heise_fsw
-docker run -p 49160:8080 -p 49170:9990 --link fsw:fsw --name bpms -d psteiner/heise_bpm
+docker run -p 49222:22 -p 49260:8080 -p 49270:9990 -h fsw --name fsw -d psteiner/heise_fsw
+docker run -p 49122:22 -p 49160:8080 -p 49170:9990 --link fsw:fsw --name bpms -d psteiner/heise_bpm
 
