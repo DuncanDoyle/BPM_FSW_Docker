c8664ee970d7b5eef59fbd9fa4a30234304634b8
Juergen Hoffmann <hoffmann@apache.org>
2014-07-10 16:00:05 +0200

    make it easier to start multiple environments
    Currently you can only start one environment. With this change it
    should be possible to start multiple environments at the same time.

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker run -p 49222:22 -p 49260:8080 -p 49270:9990 -h fsw --name fsw -d psteiner/heise_fsw
-docker run -p 49122:22 -p 49160:8080 -p 49170:9990 --link fsw:fsw --name bpms -d psteiner/heise_bpm
+# docker run -p 49222:22 -p 49260:8080 -p 49270:9990 -h fsw --name fsw -d psteiner/heise_fsw
+# docker run -p 49122:22 -p 49160:8080 -p 49170:9990 --link fsw:fsw --name bpms -d psteiner/heise_bpm
+
+ID=$(docker run -P -h fsw -d psteiner/heise_fsw)
+docker run -P --link fsw:$ID -d psteiner/heise_bpm
 
