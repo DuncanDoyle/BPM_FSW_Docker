45fd3a5f74853a4d2cb1b551c7cd801360091f7a
Juergen Hoffmann <hoffmann@apache.org>
2014-07-11 00:29:14 +0200

    review stopped containers before creating new ones
    Before creating a new named container we need to check whether there
    are existing stopped containers with the name fsw. If we find one then
    start it otherwise create a new one

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ ! $( docker ps | grep fsw | wc -l ) -gt 0 ]; then 
-  docker run -P -h fsw --name fsw -d psteiner/heise_fsw
+  if [ ! $( docker ps -a | grep fsw | wc -l ) -gt 0 ]; then
+    docker run -P -h fsw --name fsw -d psteiner/heise_fsw
+  else
+    docker start fsw
+  fi
 fi
 
 docker run -P --rm --link fsw:fsw psteiner/heise_bpm
