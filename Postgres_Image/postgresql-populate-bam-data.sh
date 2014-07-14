07903a975c42bbf0c078b1bda8f0c40673efe2fe
Patrick Steiner <patrick@steiner-buchholz.de>
2014-07-15 00:32:06 +0200

    working on postgres

--- Postgres_Image/postgresql-populate-bam-data.sh
+++ Postgres_Image/postgresql-populate-bam-data.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+service postgresql-9.3 start
+
 psql -d bamdb -U bam -f /home/jboss/images/postgresql-test.sql
+
+service postgresql-9.3 stop
+
+runuser -l postgres -c '/usr/pgsql-9.3/bin/postgres -D /var/lib/pgsql/9.3/data -c config_file=/var/lib/pgsql/9.3/data/postgresql.conf'
+
